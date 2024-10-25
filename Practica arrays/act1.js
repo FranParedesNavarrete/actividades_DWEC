cf5131de7a20a6b6f211b317d414a2afa37069ee
fran <kfran697@gmail.com>
2024-10-25 20:16:49 +0200

    Actividad2 terminada

--- Practica arrays/act1.js
+++ Practica arrays/act1.js
@@ -1,3 +1,8 @@
+/* 
+* Mostrar por consola 50 combinaciones aleatorias de la lotería primitiva. 
+* Las combinaciones son 6 números del 1 al 49, pero debe tenerse en cuenta que no se pueden repetir los números en una misma combinación.
+*/
+
 let combinacion = new Array();
 
 for (let i = 0; i < 50; i++) {
